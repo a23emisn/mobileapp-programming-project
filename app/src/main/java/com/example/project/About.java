99122babd150b0c93ab19f365a1feb62d2a972bb
a23emisn <a23emisn@student.his.se>
2024-05-07 10:32:12 +0200

    Added a WebView element in About

--- app/src/main/java/com/example/project/About.java
+++ app/src/main/java/com/example/project/About.java
@@ -3,13 +3,21 @@ package com.example.project;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 public class About extends AppCompatActivity {
+
+    private WebView myWebView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
+        myWebView = (WebView) findViewById(R.id.about);
+        myWebView.setWebViewClient(new WebViewClient());
+        myWebView.loadUrl("///android_asset/about.html");
+
     }
 
 }

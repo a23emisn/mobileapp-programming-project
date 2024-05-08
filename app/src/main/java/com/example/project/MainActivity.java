bf3b7d26b24bb38876b0fa806b2d2692d612ea5c
a23emisn <a23emisn@student.his.se>
2024-05-08 09:32:25 +0200

    Mushrooms can now be fetched and displayed, and added to the recyclerview, implemented GSON parsing.

--- app/src/main/java/com/example/project/MainActivity.java
+++ app/src/main/java/com/example/project/MainActivity.java
@@ -2,13 +2,27 @@ package com.example.project;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
-
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
-public class MainActivity extends AppCompatActivity {
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+import java.lang.reflect.Type;
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity implements JsonTask.JsonTaskListener {
+
+    private final String JSON_URL = "https://mobprog.webug.se/json-api?login=a23emisn";
+    private ArrayList<Mushroom> mushrooms = new ArrayList<>();
+    private RecyclerViewAdapter adapter;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,6 +30,20 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        adapter = new RecyclerViewAdapter(this, mushrooms, new RecyclerViewAdapter.OnClickListener() {
+            @Override
+            public void onClick(Mushroom mushroom) {
+                Toast.makeText(MainActivity.this, mushroom.getName(), Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        RecyclerView view = findViewById(R.id.recycler_view);
+        view.setLayoutManager(new LinearLayoutManager(this));
+        view.setAdapter(adapter);
+
+        String url = "https://mobprog.webug.se/json-api?login=a23emisn";
+        new JsonTask(this).execute(url);
     }
 
     @Override
@@ -33,6 +61,15 @@ public class MainActivity extends AppCompatActivity {
             return true;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onPostExecute(String json) {
+        Gson gson = new Gson();
+        Type type = new TypeToken<ArrayList<Mushroom>>() {}.getType();
+        ArrayList<Mushroom> listOfMushrooms = gson.fromJson(json, type);
+        mushrooms.addAll(listOfMushrooms);
+        adapter.notifyDataSetChanged();
     }
 
 }
